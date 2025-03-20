4f44e44c3a60e78ab80445217176936f9508c5de
hieurunsystem <hieuln1@runsystem.net>
2025-03-20 09:32:13 +0700

    sửa định dạng mail và giới hạn thời gian hiệu lực của pin, check quá số lần nhập

--- server/pkg/email/email.go
+++ server/pkg/email/email.go
@@ -22,14 +22,54 @@ MIME-Version: 1.0
 Content-Type: text/html; charset="UTF-8"
 
 <html>
-	<body style="font-family: Arial, sans-serif; line-height: 1.6;">
+	<body style="font-family: Arial, sans-serif;">
 		<p>Kính gửi <strong>%s</strong>,</p>
 		<p>Mã PIN xác nhận của bạn là:</p>
 		<p style="font-size: 20px; font-weight: bold; color: #ff0000;">%d</p>
-		<p>Mã PIN này sẽ hết hạn sau <strong>5 phút</strong>. Vui lòng không chia sẻ mã PIN này với bất kỳ ai.</p>
+		<p>Mã PIN này sẽ hết hạn sau <strong>1 phút</strong>. Vui lòng không chia sẻ mã PIN này với bất kỳ ai.</p>
 		<p>Trân trọng</p>
 	</body>
 </html>`, email, email, pin)
+
+	err := smtp.SendMail(
+		"smtp.gmail.com:587",
+		auth,
+		"golangtraining2025@gmail.com",
+		[]string{email},
+		[]byte(msg),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func SendMailWarning(email string) {
+	auth := smtp.PlainAuth(
+		"",
+		"golangtraining2025@gmail.com",
+		"vowhfgfectpvypos",
+		"smtp.gmail.com",
+	)
+
+	msg := fmt.Sprintf(`From: golangtraining2025@gmail.com
+To: %s
+Subject: Cảnh báo: Nhập mã PIN sai quá 5 lần
+MIME-Version: 1.0
+Content-Type: text/html; charset="UTF-8"
+
+<html>
+	<body style="font-family: Arial, sans-serif; ">
+		<p>Kính gửi <strong>%s</strong>,</p>
+		<p>Chúng tôi phát hiện tài khoản của bạn đã nhập mã PIN sai quá <strong>5 lần</strong>.</p>
+		<p>Vui lòng kiểm tra xem bạn có thực hiện các lần nhập này không hoặc tài khoản của bạn có đang bị truy cập trái phép hay không.</p>
+		<p style="font-size: 16px; font-weight: bold; color: #ff0000;">Nếu không phải bạn, hãy thực hiện các bước sau:</p>
+		<ul>
+			<li>Thay đổi mật khẩu ngay lập tức.</li>
+		</ul>
+		<p>Nếu cần hỗ trợ, vui lòng truy cập <strong>web chúng tôi</strong>.</p>
+	</body>
+</html>`, email, email)
 
 	err := smtp.SendMail(
 		"smtp.gmail.com:587",
