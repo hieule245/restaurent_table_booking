02993518b6dafc2ff217f50c86b7488b8d63b1aa
lamtvt <lamtvt@runsystem.net>
2025-03-25 11:17:21 +0700

    test change to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import Main from "./components/Main/Main";
 import TestAPI from "./Test/TestAPI";
 function App() {
   return (
-    // <Main />
-    <div>
-      <TestAPI />
-    </div>
+    <Main />
+    // <div>
+    //   <TestAPI />
+    // </div>
   );
 }
 
