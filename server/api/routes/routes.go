422275b9e30fe8156ef235bcad42cca514e0c6eb
lamtvt <lamtvt@runsystem.net>
2025-04-10 09:04:55 +0700

    merge super advance with no error anymore, just somethings not work correstly

--- server/api/routes/routes.go
+++ server/api/routes/routes.go
@@ -11,23 +11,6 @@ import (
 // Routes định nghĩa tất cả các route của ứng dụng.
 func Routes(server *gin.Engine) {
 	server.GET("/", sayHi)
-	// Các route liên quan đến owner: Tìm kiếm bàn trống & thời gian đã đặt
-	server.GET("/restaurants/:restaurant_id/tables/available", services.SearchAvailableTablesHandler)
-
-	server.GET("/restaurants/:restaurant_id/tables/:table_id/booked-times", services.GetBookedTimesHandler)
-
-	// Các route công khai cho restaurant và table
-	server.GET("/restaurants", services.GetAllRestaurants)
-
-	// filter table with time start, time end, date
-	server.GET("/restaurant/:restaurant_id/available-tables", services.GetAvailableTables)
-
-	// router book table
-	server.GET("/booking-history", services.GetBookingHistoryByCustomerID)
-	// edit reservation for customer
-	server.PUT("/reservation/:reservation_id", services.EditReservation)
-	// delete reservation for customer
-	server.DELETE("/reservation/:reservation_id", services.CancelReservation)
 
 	restaurant := server.Group("/restaurant")
 	{
