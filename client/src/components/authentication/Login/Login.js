b587c20fbfc67389094b7f37ac099b2bce831f68
lamtvt <lamtvt@runsystem.net>
2025-04-21 16:53:47 +0700

    a

--- client/src/components/authentication/Login/Login.js
+++ client/src/components/authentication/Login/Login.js
@@ -11,6 +11,7 @@ import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import "./Login.style.css";
+import { loginSchema } from "../../../validations/AccountSchema";
 
 const LoginPage = () => {
   const navigate = useNavigate();
